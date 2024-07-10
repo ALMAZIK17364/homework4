07830586571a6836db76e3d3ef1b78dd7649d6f8
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-07-11 00:18:28 +0800

    Add files via upload
    Практическая работа по уроку "Организация программ и методы строк."

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input("Ввдеите... что-то.. \nХоть что-нибудь, пожалуйста.\n")
+print(len(my_string))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[len(my_string)-1])
